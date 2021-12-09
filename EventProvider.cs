efb5ddbb6fde4d215e271a5dc2499d873b3d1694
davidTigris <g.musset@mailbox.org>
2021-12-09 18:39:37 +0100

    Initial commit

---
+++ EventProvider.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Timers;
+
+namespace EventPatternStudy
+{
+    public class EventProvider
+    {
+        private readonly System.Timers.Timer _timer;
+        private int _eventCounter = 1;
+        public event EventHandler<NewNumberEventArgs> NewNumber;
+
+        public EventProvider()
+        {
+            _timer = new System.Timers.Timer();
+            _timer.Elapsed += new ElapsedEventHandler(OnTimerElapsed);
+            _timer.Interval = 3000;
+            _timer.AutoReset = true;
+        }
+
+        private void OnTimerElapsed(object sender, ElapsedEventArgs args)
+        {
+            var eventArgs = new NewNumberEventArgs(_eventCounter++);
+            NewNumber?.Invoke(this, eventArgs);
+            if (eventArgs.Cancel)
+            {
+                Console.WriteLine($"Event '{nameof(NewNumber)}' was canceled.");
+                Stop();
+                
+            }
+
+            ;
+        }
+
+        public void Start() => _timer.Start();
+
+        private void Stop() => _timer.Stop();
+    }
+
+    public class NewNumberEventArgs : EventArgs
+    {
+        public NewNumberEventArgs(int eventNumber)
+        {
+            EventNumber = eventNumber;
+            TimeStamp = DateTime.Now;
+        }
+
+        public int EventNumber { get; }
+        public DateTime TimeStamp { get; }
+        public bool Cancel { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using EventPatternStudy;
+
+void OnNewNumber(object sender, NewNumberEventArgs args)
+{
+    Console.WriteLine($"{args.TimeStamp}: {args.EventNumber}. event occured");
+    if (args.EventNumber % 5 == 0) args.Cancel = true;
+}
+
+EventProvider eventProvider = new();
+eventProvider.NewNumber += OnNewNumber;
+eventProvider.Start();
+
+Console.ReadKey();
